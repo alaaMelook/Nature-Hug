0fbecc7bd9ec7baf0241b68a64e7b30d45f18fcc
Katherine Ashraf <katherineAshraf@gmail.com>
2025-12-10 12:26:19 +0200

    images fix

--- app/[lang]/(store)/about-us/page.tsx
+++ app/[lang]/(store)/about-us/page.tsx
@@ -20,7 +20,7 @@ export default function AboutPage() {
         Zap,
     };
     return (
-        <main className="bg-white text-gray-800 overflow-x-hidden">
+        <main className="bg-gray-50 text-gray-800 overflow-x-hidden">
             {/* ===== Hero Section ===== */}
             <section className="text-center py-24 px-6">
                 <motion.h1
@@ -85,11 +85,11 @@ export default function AboutPage() {
                 transition={{ duration: 0.6 }}
             >
 
-                <div className="bg-primary-100 border border-gray-100 rounded-xl px-10 md:px-15 py-10 text-sm text-gray-800 flex flex-col md:flex-row md:gap-30 items-center justify-center gap-10">
-                    <Image src="https://reqrsmboabgxshacmkst.supabase.co/storage/v1/object/public/nature-hug/kera-photo.png" alt="Kera Photo"
-                        width={200} height={200}
-                        style={{ objectFit: 'cover' }}
-                        className="rounded-full border-5 border-primary-900 " >
+                <div className="bg-primary-100 border border-gray-100 rounded-xl px-10 md:px-15 py-10 text-sm text-gray-800 flex flex-col md:flex-row md:gap-50 items-center justify-center gap-10 md:my-10">
+                    <Image src="https://reqrsmboabgxshacmkst.supabase.co/storage/v1/object/public/nature-hug/products/Gemini_Generated_Image_aig2xtaig2xtaig2.png" alt="KeraCream Photo"
+                        width={800} height={800}
+
+                        className="rounded-full border-2 border-primary-800 scale-180 md:scale-250 w-20 h-20 md:w-24 md:h-24 " >
                     </Image>
                     <span className='w-full md:w-2/5 text-center md:text-start text-lg'>
 
