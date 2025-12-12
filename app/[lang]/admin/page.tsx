c23d857f15bf0d0b6d30bae15128b8ff7327a78f
Katherine Ashraf <katherineAshraf@gmail.com>
2025-12-12 17:02:39 +0200

    faster rendering

--- app/[lang]/admin/page.tsx
+++ app/[lang]/admin/page.tsx
@@ -3,8 +3,10 @@ import { GetSidebarStats } from "@/domain/use-case/admin/getSidebarStats";
 import { AdminDashboardScreen } from "@/ui/client-screens/admin/admin-dashboard-screen";
 
 export default async function AdminDashboard() {
-    const dashboard = await new GetAdminDashboardStats().execute();
-    const actionStats = await new GetSidebarStats().execute();
+    const [dashboard, actionStats] = await Promise.all([
+        new GetAdminDashboardStats().execute(),
+        new GetSidebarStats().execute()
+    ]);
 
     return (
         <AdminDashboardScreen dashboard={dashboard} actionStats={actionStats} />
